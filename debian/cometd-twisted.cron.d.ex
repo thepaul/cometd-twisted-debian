6e1297bfda0ddf28f05eb05abe228c2dba097a16
paul cannon <paul@riptano.com>
2010-12-01 10:51:28 -0600

    first pass at packaging

--- debian/cometd-twisted.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the cometd-twisted package
-#
-0 4	* * *	root	[ -x /usr/bin/cometd-twisted_maintenance ] && /usr/bin/cometd-twisted_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(cometd-twisted):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="cometd-twisted" command="/usr/bin/cometd-twisted"
