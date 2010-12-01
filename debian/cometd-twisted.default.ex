e291bd8bd347c5455b4716454c2273c2051a8e1a
paul cannon <paul@riptano.com>
2010-12-01 10:29:36 -0600

    bare dh_make output
    from dh_make -s -c bsd -r -i

---
+++ debian/cometd-twisted.default.ex
@@ -0,0 +1,10 @@
+# Defaults for cometd-twisted initscript
+# sourced by /etc/init.d/cometd-twisted
+# installed at /etc/default/cometd-twisted by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(cometd-twisted):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="cometd-twisted" command="/usr/bin/cometd-twisted"
